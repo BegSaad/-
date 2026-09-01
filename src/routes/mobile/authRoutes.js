0b9aa1a3c96f634e6dd96f2249f43e3b2c2df893
BegSaad <mohdsaadstp2019@gmail.com>
2026-09-01 19:53:12 +0530

    h

--- src/routes/mobile/authRoutes.js
+++ src/routes/mobile/authRoutes.js
@@ -10,4 +10,42 @@ authRouter.post('/register',(req,res)=>{
     console.log(email)
 
 })
+authRouter.get('/query/:id', (req, res) => {
+    const { id } = req.params;
+
+    console.log(id);
+
+    res.json({
+        id: id
+    });
+});
+//pass data via a body
+
+authRouter.post("/body",(req,res)=>{
+    const {name,age}=req.body;
+    res.json({
+        name,
+        age
+    })
+    console.log(name);
+    console.log(age)
+})
+//pass data via url parameters
+authRouter.get("/params/:id",(req,res)=>{
+    const{id}=req.params
+res.json({
+    id
+})
+console.log(id)
+})
+
+
+//pass data via query parameters
+authRouter.get("/queryparameter",(req,res)=>{
+    const {name}=req.query
+console.log(name)
+res.json({
+    name
+})
+})
 export default authRouter
